5eadd8cc9e3cb6a52c5f3dc022343d54cb992bb6
rturnbull <robert.turnbull@unimelb.edu.au>
2024-05-28 10:59:35 +1000

    :zap: adding way to draw predictions from a model

--- drawyolo/main.py
+++ drawyolo/main.py
@@ -4,16 +4,29 @@ from rich.console import Console
 console = Console()
 import typer
 
-from .draw import draw_box_on_image
+from .draw import draw_box_on_image_with_labels, draw_box_on_image_with_model
 
 app = typer.Typer()
 
 
 @app.command()
-def drawyolo(image: Path, labels: Path, output: Path, classes:str):
+def drawyolo(
+    image: Path, 
+    output: Path,
+    labels: Path=None, 
+    weights: Path=None, 
+    classes:str=None,
+    highest:bool=False,
+):
     """
     Draw boxes on image
     """
-    classes = classes.split(',')
+    if classes is not None:
+        classes = classes.split(',')
 
-    draw_box_on_image(image, labels, output, classes)
+    if labels is not None:
+        draw_box_on_image_with_labels(image, labels, output, classes)
+    elif weights is not None:
+        draw_box_on_image_with_model(image, weights, output, classes=classes, highest=highest)
+    else:
+        print("Please provide either labels or a model")
