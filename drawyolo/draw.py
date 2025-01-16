f5752a84b966e3221c5e3b4b1e44f9fda0b4d00e
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2025-01-17 10:06:40 +1100

    :art: black reformatting'

--- drawyolo/draw.py
+++ drawyolo/draw.py
@@ -3,7 +3,13 @@ import random
 import numpy as np
 from pathlib import Path
 
-def rescale_sizes(original_width:int, original_height:int, width: int|None = None, height: int|None = None) -> tuple[int, int]:
+
+def rescale_sizes(
+    original_width: int,
+    original_height: int,
+    width: int | None = None,
+    height: int | None = None,
+) -> tuple[int, int]:
     """
     The size of the image after rescaling.
 
@@ -24,7 +30,9 @@ def rescale_sizes(original_width:int, original_height:int, width: int|None = Non
     return width, height
 
 
-def rescale_image(image: np.ndarray, width: int|None = None, height: int|None = None) -> np.ndarray:
+def rescale_image(
+    image: np.ndarray, width: int | None = None, height: int | None = None
+) -> np.ndarray:
     """Rescale an image based on given width and height.
 
     - If both `width` and `height` are None or 0, return the original image.
@@ -39,8 +47,13 @@ def rescale_image(image: np.ndarray, width: int|None = None, height: int|None = 
     return cv2.resize(image, (width, height), interpolation=cv2.INTER_AREA)
 
 
-def save_image(image:np.ndarray, output:str|Path|None, width:int|None=None, height:int|None=None):
-    """ Save image to file """
+def save_image(
+    image: np.ndarray,
+    output: str | Path | None,
+    width: int | None = None,
+    height: int | None = None,
+):
+    """Save image to file"""
     if output:
         output = Path(output)
         output.parent.mkdir(parents=True, exist_ok=True)
@@ -48,41 +61,72 @@ def save_image(image:np.ndarray, output:str|Path|None, width:int|None=None, heig
         cv2.imwrite(str(output), image)
 
 
-def default_line_thickness(width:int, height:int) -> int:
+def default_line_thickness(width: int, height: int) -> int:
     return round(0.002 * (height + width) / 2) + 1
 
 
-def default_line_thickness_before_rescale(original_width:int, original_height:int, width:int|None=None, height:int|None=None) -> int:
-    original_thickness_no_rescale = default_line_thickness(original_width, original_height)
-    final_width, final_height = rescale_sizes(original_width, original_height, width, height)
+def default_line_thickness_before_rescale(
+    original_width: int,
+    original_height: int,
+    width: int | None = None,
+    height: int | None = None,
+) -> int:
+    original_thickness_no_rescale = default_line_thickness(
+        original_width, original_height
+    )
+    final_width, final_height = rescale_sizes(
+        original_width, original_height, width, height
+    )
     final_thickness = default_line_thickness(final_width, final_height)
-    return round(original_thickness_no_rescale ** 2 / final_thickness)
+    return round(original_thickness_no_rescale**2 / final_thickness)
 
 
-def plot_one_box(x, image, color=None, label:str=None, line_thickness:int|None=None):
-    """ Plots one bounding box on image img """
-    line_thickness = line_thickness or default_line_thickness(image.shape[1], image.shape[0])
+def plot_one_box(
+    x, image, color=None, label: str = None, line_thickness: int | None = None
+):
+    """Plots one bounding box on image img"""
+    line_thickness = line_thickness or default_line_thickness(
+        image.shape[1], image.shape[0]
+    )
     color = color or [random.randint(0, 255) for _ in range(3)]
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
     cv2.rectangle(image, c1, c2, color, thickness=line_thickness, lineType=cv2.LINE_AA)
     if label:
         tf = max(line_thickness - 1, 1)  # font thickness
-        t_size = cv2.getTextSize(label, 0, fontScale=line_thickness / 3, thickness=tf)[0]
+        t_size = cv2.getTextSize(label, 0, fontScale=line_thickness / 3, thickness=tf)[
+            0
+        ]
         c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
         cv2.rectangle(image, c1, c2, color, -1, cv2.LINE_AA)  # filled
-        cv2.putText(image, label, (c1[0], c1[1] - 2), 0, line_thickness / 3,
-                    [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+        cv2.putText(
+            image,
+            label,
+            (c1[0], c1[1] - 2),
+            0,
+            line_thickness / 3,
+            [225, 255, 255],
+            thickness=tf,
+            lineType=cv2.LINE_AA,
+        )
 
 
-def make_colors(count:int):
-    """ Generate random colors """
+def make_colors(count: int):
+    """Generate random colors"""
     random.seed(42)
-    colors = [[random.randint(0, 255) for _ in range(3)]
-              for _ in range(count)]
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(count)]
     return colors
 
 
-def draw_box_on_image_with_labels(image:Path, labels:Path, output:Path, classes:list[str], colors:list[str]=None, width:int|None=None, height:int|None=None, line_thickness:int|None=None) -> np.ndarray:
+def draw_box_on_image_with_labels(
+    image: Path,
+    labels: Path,
+    output: Path,
+    classes: list[str],
+    colors: list[str] = None,
+    width: int | None = None,
+    height: int | None = None,
+    line_thickness: int | None = None,
+) -> np.ndarray:
     """
     Adds rectangle boxes on the images.
     """
@@ -93,10 +137,12 @@ def draw_box_on_image_with_labels(image:Path, labels:Path, output:Path, classes:
         image = cv2.imread(str(image))
         original_height, original_width = image.shape[:2]
     except Exception as e:
-        print(f'Cannot read image: {e}')
+        print(f"Cannot read image: {e}")
         return
-    
-    line_thickness = line_thickness or default_line_thickness_before_rescale(original_width, original_height, width, height)
+
+    line_thickness = line_thickness or default_line_thickness_before_rescale(
+        original_width, original_height, width, height
+    )
 
     # Get Labels
     labels = Path(labels)
@@ -105,25 +151,40 @@ def draw_box_on_image_with_labels(image:Path, labels:Path, output:Path, classes:
         staff = line.split()
         class_idx = int(staff[0])
 
-        x_center, y_center, w, h = float(staff[1])*original_width, float(staff[2])*original_height, float(staff[3])*original_width, float(staff[4])*original_height
-        x1 = round(x_center-w/2)
-        y1 = round(y_center-h/2)
-        x2 = round(x_center+w/2)
-        y2 = round(y_center+h/2)
+        x_center, y_center, w, h = (
+            float(staff[1]) * original_width,
+            float(staff[2]) * original_height,
+            float(staff[3]) * original_width,
+            float(staff[4]) * original_height,
+        )
+        x1 = round(x_center - w / 2)
+        y1 = round(y_center - h / 2)
+        x2 = round(x_center + w / 2)
+        y2 = round(y_center + h / 2)
 
         plot_one_box(
-            [x1, y1, x2, y2], 
-            image, 
+            [x1, y1, x2, y2],
+            image,
             color=colors[class_idx],
-            label=classes[class_idx], 
+            label=classes[class_idx],
             line_thickness=line_thickness,
         )
-    
+
     save_image(image, output, width=width, height=height)
     return image
 
 
-def draw_box_on_image_with_yolo_result(image:Path, results, output:Path, classes:list[str]=None, colors:list[str]=None, highest:bool=False, width:int|None=None, height:int|None=None, line_thickness:int|None=None) -> np.ndarray:
+def draw_box_on_image_with_yolo_result(
+    image: Path,
+    results,
+    output: Path,
+    classes: list[str] = None,
+    colors: list[str] = None,
+    highest: bool = False,
+    width: int | None = None,
+    height: int | None = None,
+    line_thickness: int | None = None,
+) -> np.ndarray:
     colors = colors or make_colors(len(classes))
 
     image = cv2.imread(str(image))
@@ -140,16 +201,18 @@ def draw_box_on_image_with_yolo_result(image:Path, results, output:Path, classes
         boxes_list = [best_boxes]
 
     original_height, original_width = image.shape[:2]
-    line_thickness = line_thickness or default_line_thickness_before_rescale(original_width, original_height, width, height)
+    line_thickness = line_thickness or default_line_thickness_before_rescale(
+        original_width, original_height, width, height
+    )
 
     for boxes in boxes_list:
         class_idx = boxes.cls[0].int().item()
         xyxy = boxes.xyxy.cpu()[0]
         plot_one_box(
-            xyxy, 
-            image, 
+            xyxy,
+            image,
             color=colors[class_idx],
-            label=classes[class_idx], 
+            label=classes[class_idx],
             line_thickness=line_thickness,
         )
 
@@ -157,8 +220,19 @@ def draw_box_on_image_with_yolo_result(image:Path, results, output:Path, classes
     return image
 
 
-def draw_box_on_image_with_model(image:Path, weights:Path|str, output:Path, res:int=1280, classes:list[str]=None, colors:list[str]=None, highest:bool=False, width:int|None=None, height:int|None=None, line_thickness:int|None=None) -> np.ndarray:
-    """ Draw boxes on image using YOLO model """
+def draw_box_on_image_with_model(
+    image: Path,
+    weights: Path | str,
+    output: Path,
+    res: int = 1280,
+    classes: list[str] = None,
+    colors: list[str] = None,
+    highest: bool = False,
+    width: int | None = None,
+    height: int | None = None,
+    line_thickness: int | None = None,
+) -> np.ndarray:
+    """Draw boxes on image using YOLO model"""
     from ultralytics import YOLO
 
     model = YOLO(str(weights))
@@ -170,13 +244,13 @@ def draw_box_on_image_with_model(image:Path, weights:Path|str, output:Path, res:
     assert len(results) == 1
 
     return draw_box_on_image_with_yolo_result(
-        image, 
-        results[0], 
-        output=output, 
-        classes=classes, 
-        colors=colors, 
-        highest=highest, 
-        width=width, 
-        height=height, 
+        image,
+        results[0],
+        output=output,
+        classes=classes,
+        colors=colors,
+        highest=highest,
+        width=width,
+        height=height,
         line_thickness=line_thickness,
     )
--- drawyolo/main.py
+++ drawyolo/main.py
@@ -5,27 +5,56 @@ from .draw import draw_box_on_image_with_labels, draw_box_on_image_with_model
 
 app = typer.Typer()
 
+
 @app.command()
 def drawyolo(
-    image: Path=typer.Argument(..., help="Input image path"),
-    output: Path=typer.Argument(..., help="Output image path"),
-    labels: Path=typer.Option(None, help="Path to labels text file in YOLO format"),
-    weights: Path=typer.Option(None, help="Path to YOLO model weights file if not using labels text file"),
-    classes:str=typer.Option(None, help="Class names separated by comma"),
-    highest:bool=typer.Option(False, help="Only draw the box with the highest confidence"),
-    width:int=typer.Option(None, help="Width of the output image if you wish to resize it"),
-    height:int=typer.Option(None, help="Height of the output image if you wish to resize it"),
-    line_thickness:int=typer.Option(None, help="Thickness of the box lines. If not provided, it will be calculated based on the final image size."),
+    image: Path = typer.Argument(..., help="Input image path"),
+    output: Path = typer.Argument(..., help="Output image path"),
+    labels: Path = typer.Option(None, help="Path to labels text file in YOLO format"),
+    weights: Path = typer.Option(
+        None, help="Path to YOLO model weights file if not using labels text file"
+    ),
+    classes: str = typer.Option(None, help="Class names separated by comma"),
+    highest: bool = typer.Option(
+        False, help="Only draw the box with the highest confidence"
+    ),
+    width: int = typer.Option(
+        None, help="Width of the output image if you wish to resize it"
+    ),
+    height: int = typer.Option(
+        None, help="Height of the output image if you wish to resize it"
+    ),
+    line_thickness: int = typer.Option(
+        None,
+        help="Thickness of the box lines. If not provided, it will be calculated based on the final image size.",
+    ),
 ):
     """
     Draw boxes on image from a YOLO model or labels text file in YOLO format.
     """
     if classes is not None:
-        classes = classes.split(',')
+        classes = classes.split(",")
 
     if labels is not None:
-        draw_box_on_image_with_labels(image, labels, output, classes, width=width, height=height, line_thickness=line_thickness)
+        draw_box_on_image_with_labels(
+            image,
+            labels,
+            output,
+            classes,
+            width=width,
+            height=height,
+            line_thickness=line_thickness,
+        )
     elif weights is not None:
-        draw_box_on_image_with_model(image, weights, output, classes=classes, highest=highest, width=width, height=height, line_thickness=line_thickness)
+        draw_box_on_image_with_model(
+            image,
+            weights,
+            output,
+            classes=classes,
+            highest=highest,
+            width=width,
+            height=height,
+            line_thickness=line_thickness,
+        )
     else:
         raise typer.BadParameter("You must provide either --labels or --weights.")
